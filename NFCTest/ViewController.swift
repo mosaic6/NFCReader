3a10d91cb8354556dd816b191f6622317a85dde5
Joshua Walsh <mosaic6@mac.com>
2017-06-11 21:39:19 -0400

    Initial Commit

---
+++ NFCTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFCTest
+//
+//  Created by Joshua Walsh on 6/11/17.
+//  Copyright © 2017 Joshua Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
